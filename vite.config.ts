f0681d55ea5417167597fe56bac96a6b06525c39
Candid Dauth <cdauth@cdauth.eu>
2022-04-17 15:21:51 +0200

    Migrate to Vite and publish ESM bundle

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+import dtsPlugin from 'vite-plugin-dts';
+
+export default defineConfig({
+	plugins: [
+		cssInjectedByJsPlugin(),
+		dtsPlugin()
+	],
+	build: {
+		sourcemap: true,
+		minify: false,
+		lib: {
+			entry: './src/index.ts',
+			name: 'L.HighlightableLayers',
+			fileName: (format) => `L.HighlightableLayers.${format === 'umd' ? 'js' : 'mjs'}`
+		},
+		rollupOptions: {
+			output: {
+				globals: {
+					'leaflet': 'L'
+				}
+			},
+			external: ['leaflet']
+		}
+	}
+});
